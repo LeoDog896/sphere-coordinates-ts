714457ceda2c19fb0dbd379bc3f97eaebff34f56
Tristan F <LeoDog896@hotmail.com>
2023-04-21 15:32:59 +0000

    feat: initial library

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # sphere-coordinates-ts
 
-implementation of [Jonathan Kogan's method for equidistant sphere coordinates](https://scholar.rose-hulman.edu/cgi/viewcontent.cgi?article=1387&context=rhumj) in TypeScript for Deno and Node
+implementation of [Jonathan Kogan's method for equidistant sphere coordinates](https://scholar.rose-hulman.edu/cgi/viewcontent.cgi?article=1387&context=rhumj) in TypeScript for Deno and Node.
---
+++ index.ts
@@ -0,0 +1,24 @@
+function spherical_coordinates(x: number, y: number): number[] {
+    return [Math.cos(x) * Math.cos(y), Math.sin(x) * Math.cos(y), Math.sin(y)];
+}
+
+function NX(n: number, x: number): number[][] {
+    const pts: number[][] = [];
+    const start = -1 + 1 / (n - 1);
+    const increment = (2 - 2 / (n - 1)) / (n - 1);
+    for (let j = 0; j < n; j++) {
+        const s = start + j * increment;
+        pts.push(
+            spherical_coordinates(
+                s * x,
+                Math.PI / 2 * Math.sign(s) * (1 - Math.sqrt(1 - Math.abs(s)))
+            )
+        );
+    }
+
+    return pts;
+}
+
+export function generate_points(n: number): number[][] {
+    return NX(n, 0.1 + 1.2 * n);
+}
